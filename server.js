83ccad930b6ea22f2f4dc22c2cdc1bf330b14b0a
Joe Talafous <joetalafous@Joes-MBP.attlocal.net>
2023-02-08 15:22:43 -0800

    test

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+import express from 'express'
+
+//initiate express
+const app = express()
+app.use(express.json())
+
+app.get('*', checkJwt, (req, res) => {
+  res.json({ msg: 'not a valid route' })
+  console.log('* hit')
+})
+
+//start server
+const port = process.env.PORT || 4242
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`)
+})
+
+//test push
