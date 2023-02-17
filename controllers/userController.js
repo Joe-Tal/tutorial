03eda51f3aadd1c7cedde171648fcce891133749
Joe Talafous <joetalafous@Joes-MBP.attlocal.net>
2023-02-17 15:05:44 -0800

    updating from session 2

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+import { StatusCodes } from 'http-status-codes'
+import cloudDB from '../cloudDB.js'
+
+const upsertUser = async (req, res) => {
+  const db = cloudDB.get().db('tutorial')
+
+  const email = 'steven.talafous@gmail.com'
+  const set = {
+    email: email,
+    phone: '7327799640',
+    address: 'miami',
+  }
+
+  await db
+    .collection('firstCollection')
+    //1. Finding the document
+    .updateOne({ email: email }, { $set: set }, { upsert: true })
+
+  console.log('upsert user for')
+  res.status(StatusCodes.OK).json({ msg: 'upsert user' })
+}
+
+export { upsertUser }
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from 'express'
+import cloudDB from './cloudDB.js'
+import userRoutes from './routes/userRoutes.js'
 
 //initiate express
 const app = express()
 app.use(express.json())
 
-app.get('*', checkJwt, (req, res) => {
+await cloudDB.connect()
+
+//routes
+app.get('/', (req, res) => {
+  res.json({ msg: 'hello world' })
+})
+app.use('/user', userRoutes)
+app.get('*', (req, res) => {
   res.json({ msg: 'not a valid route' })
   console.log('* hit')
 })
